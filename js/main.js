37b48a2dd0249b48e16c84d62215e90397cb82c0
Revan-000 <61224041+revangozelov@users.noreply.github.com>
2021-06-29 10:49:17 +0400

    wsdas

--- js/main.js
+++ js/main.js
@@ -10,14 +10,14 @@ jQuery(document).ready(function($) {
 
 	'use strict';
 
-    var top_header = $('.parallax-content');
+  /*   var top_header = $('.parallax-content');
     top_header.css({'background-position':'center center'}); // better use CSS
 
     $(window).scroll(function () {
     var st = $(this).scrollTop();
     top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
     });
-
+ */
 
     $('body').scrollspy({ 
         target: '.fixed-side-navbar',
