d006c77112c581da6bb96bb492ed9ab4e6e3a7c6
revangozelov <61224041+revangozelov@users.noreply.github.com>
2021-07-12 16:46:58 +0400

    reva

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@ var getUrlParameter = function getUrlParameter(sParam) {
   }
   return false;
 };
+
 var UrlQb = "https://app.sourcedagile.com/";
 (function () {
 
@@ -58,6 +59,14 @@ var UrlQb = "https://app.sourcedagile.com/";
            
           genSectionSingle(subdt);
         }
+     
+
+      }
+      if (alte === 'eventListAll') {
+        $('#services').show();
+
+        
+        genEventsList2();
      
 
       }
@@ -391,6 +400,133 @@ var UrlQb = "https://app.sourcedagile.com/";
   });
 
 })();
+function convertStDate(dt) {
+
+	var arr = dt.slice(0, 4);
+	var arr1 = dt.slice(4, 6);
+	var arr2 = dt.slice(6, 8);
+
+	var fns = arr + "/" + arr1 + '/' + arr2;
+
+	return fns
+}
+
+function convertStTime(dt) {
+
+	var arr = dt.slice(0, 2);
+	var arr1 = dt.slice(2, 4);
+
+
+	var fns = arr + ":" + arr1;
+
+	return fns
+}
+
+function getSingleEvent(fkId,id,header,lng,strtm,endtm) {
+
+  var ts = {
+    "kv": {
+      "id": fkId
+
+    }
+  }
+
+  $.ajax({
+    type: "POST",
+    url: UrlQb + "api/post/zd/traininghub/getCertificationDescription",
+    data: JSON.stringify(ts), // now data come in this function
+    contentType: "application/json; charset=utf-8",
+    crossDomain: true,
+    dataType: "json",
+    success: function (data, status, jqXHR) {
+
+      var dat = data.tbl[0].r
+        var logo = dat[0].logo
+   
+      $("#certificatie-block").append(genEventListBlock(id,logo,header,lng,strtm,endtm));
+
+   
+
+
+    }
+  })
+
+
+}
+
+
+function genEventListBlock(id,logo,header,lng,strtm,endtm){
+  return  ` <div id='${id}' class=" col-lg-4 col-md-6">
+  <div class="testimonial-item">
+  <div class="profile mt-auto">
+  <img src="${UrlQb}api/get/zdfiles/traininghub/${logo}" class="testimonial-img" alt="">
+  <h3> ${header}</h3>
+  <h4>Start Time ${strtm} <br> End Time ${endtm}</h4>
+</div>
+    <p>
+    Language ${lng}
+    </p>
+
+    <a class="getstarted scrollto" href="https://app.sourcedagile.com/login.html">Join</a>
+        
+  </div>
+</div>`
+}
+
+var lang  =[
+  {key:"en",
+    value: "English"
+   },
+  {key:"az",
+    value: "Azeribaijan"
+   },
+  {key:"ru",
+    value: "Russian"
+   },
+  
+]
+function genEventsList2(){
+  $.ajax({
+    type: "POST",
+    url: UrlQb + "api/post/zd/traininghub/getEventList4Web",
+    data: JSON.stringify(), // now data come in this function
+    contentType: "application/json; charset=utf-8",
+    crossDomain: true,
+    dataType: "json",
+    success: function (data, status, jqXHR) {
+      var dat = data.tbl[0].r
+      
+      for (let index = 0; index < dat.length; index++) {
+        var id = dat[index].id
+        var fkId = dat[index].fkCertificationId
+        var header = dat[index].eventTitle
+        var lng = dat[index].eventLang
+        var stst = dat[index].eventStatus
+        var strtm = ": "+convertStDate(dat[index].startDate)/* +""+ convertStTime(dat[index].startTime) */
+        var endtm = ": "+convertStDate(dat[index].endDate)/* +""+ convertStTime(dat[index].endTime) */
+ 
+        var dl = lang.find(x => x.key === lng).value;
+        
+
+        if(stst === "A"){
+         
+          getSingleEvent(fkId,id,header,dl,strtm,endtm)
+        }
+
+        
+
+
+      }
+
+
+    },
+
+    error: function (jqXHR, status) {
+
+    }
+  });
+}
+
 
 
 function addGenSectList(id,nm,img,desct){
